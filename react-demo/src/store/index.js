bd1a3dccd68a8429cd9f837959f6f022c15ab2a0
orelrafa <orelrafailov1998@gmail.com>
2024-12-21 19:28:15 +0200

    commit

--- react-demo/src/store/index.js
+++ react-demo/src/store/index.js
@@ -1,12 +1,22 @@
 import { createStore } from 'redux';
 
-const counterReducer = (state = { counter: 0 }, action) => {
+const initialState = { counter: 0, showCounter: true };
+
+const counterReducer = (state = initialState, action) => {
   if (action.type === 'increment') {
-    return { counter: state.counter + 1 };
+    return { ...state, counter: state.counter + 1 };
+  }
+
+  if (action.type === 'increase') {
+    return { ...state, counter: state.counter + action.amount };
   }
 
   if (action.type === 'decrement') {
-    return { counter: state.counter - 1 };
+    return { ...state, counter: state.counter - 1 };
+  }
+
+  if (action.type === 'toggle') {
+    return { ...state, showCounter: !state.showCounter };
   }
 
   return state;
