85922985b2476de5c0168a09de076bf800b3e0ce
orelrafa <orelrafailov1998@gmail.com>
2024-12-23 18:46:58 +0200

    commit

--- react-demo/src/App.js
+++ react-demo/src/App.js
@@ -1,9 +1,19 @@
-import Counter from './components/Counter';
-
+import { Fragment } from 'react';
+import { useSelector } from 'react-redux';
+import Header from './components/Header.js';
+import Counter from './components/Counter.js';
+import Auth from './components/Auth.js';
+import UserProfile from './components/UserProfile.js';
 
 function App() {
+  const isAuth = useSelector((state) => state.auth.isAuthenticated);
   return (
-    <Counter />
+    <Fragment>
+      <Header />
+      {!isAuth && <Auth />}
+      {isAuth && <UserProfile />}
+      <Counter />
+    </Fragment>
   );
 }
 
--- react-demo/src/store/index.js
+++ react-demo/src/store/index.js
@@ -1,27 +1,9 @@
-import { createStore } from 'redux';
+import { configureStore } from '@reduxjs/toolkit';
+import counterReducer from './counter.js';
+import authReducer from './auth.js';
 
-const initialState = { counter: 0, showCounter: true };
-
-const counterReducer = (state = initialState, action) => {
-  if (action.type === 'increment') {
-    return { ...state, counter: state.counter + 1 };
-  }
-
-  if (action.type === 'increase') {
-    return { ...state, counter: state.counter + action.amount };
-  }
-
-  if (action.type === 'decrement') {
-    return { ...state, counter: state.counter - 1 };
-  }
-
-  if (action.type === 'toggle') {
-    return { ...state, showCounter: !state.showCounter };
-  }
-
-  return state;
-};
-
-const store = createStore(counterReducer);
+const store = configureStore({
+  reducer: { counter: counterReducer, auth: authReducer },
+});
 
 export default store;
